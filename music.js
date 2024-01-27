11d02226efb624a1d6592623fab1ff1a37272a3a
harshdev4 <131484099+harshdev4@users.noreply.github.com>
2024-01-27 18:58:14 +0530

    Add files via upload

--- music.js
+++ music.js
@@ -1,36 +1,64 @@
 playBtn = document.querySelectorAll(".playBtn");
+controlBtn = document.querySelector(".playControlBtn");
+controlBackBtn = document.querySelector(".backControlBtn");
+controlNextBtn = document.querySelector(".nextControlBtn");
+
+let isPlaying = false;
 progressBar = document.querySelector(".bar");
 progressBarContainer = document.querySelector(".progressBar");
-isPlaying = false;
-
-let audio = new Audio("./assets/1.mp3");
-audio.load();
+let audio;
+let currentAudio;
+let index = 0;
 
 playBtn.forEach(btn => {
-    btn.addEventListener("click", function () {
-        if (!isPlaying) {
-            playBtn.forEach(function (btn) {
-                btn.innerHTML = `<i class="fa-regular fa-circle-pause"></i>`;
-            });
-            console.log("playing");
-            audio.play();
-            isPlaying = true;
-            setInterval(function () {
-                let percentage = (audio.currentTime / audio.duration) * 100;
-                progressBar.style.width = `${percentage.toFixed(2)}%`;
-            }, 1000);
-        }
-
-        else {
-            console.log("pause");
-            playBtn.forEach(function (btn) {
-                btn.innerHTML = `<i class="fa-regular fa-circle-play"></i>`;
-            });
-            audio.pause();
-            isPlaying = false;
-        }
-    })
+    btn.addEventListener("click", () => handlePlay(btn));
 });
+
+controlBtn.addEventListener("click", function () {
+    let curr = document.getElementById(currentAudio);
+
+    if (!currentAudio) {
+        playBtn[0].click();
+    }
+
+    if (isPlaying) {
+        pause(curr);
+        audio.pause();
+    }
+
+    else if (audio) {
+        audio.play();
+        isPlaying = true;
+        curr.innerHTML = `<i class="fa-regular fa-circle-pause"></i>`;
+        controlBtn.innerHTML = `<i class="fa-regular fa-circle-pause"></i>`;
+    }
+});
+
+controlBackBtn.addEventListener("click", function () {
+    if (index-1 >= 0 && currentAudio) {
+        index = currentAudio[0] - 2;
+        if (currentAudio) {
+            playBtn[index].click();
+        }
+    }
+
+    else if(!currentAudio || currentAudio[0] !== "1") {
+        playBtn[0].click();
+    }
+})
+
+controlNextBtn.addEventListener("click", function () {
+    if(!currentAudio){
+        currentAudio = playBtn[0].getAttribute("id");
+    }
+
+    if (Number(index)+1 <= playBtn.length) {
+        index = currentAudio[0];
+        if (currentAudio) {
+            playBtn[index].click();
+        }
+    }
+})
 
 progressBarContainer.addEventListener("click", function (event) {
     let rect = progressBarContainer.getBoundingClientRect();
@@ -42,4 +70,40 @@ progressBarContainer.addEventListener("click", function (event) {
     let timeStamp = (width.toFixed(2) / 100) * audio.duration;
     audio.currentTime = timeStamp;
 });
+
+
+function handlePlay(btn) {
+    if (!isPlaying || btn.getAttribute("id") !== currentAudio) {
+        if (audio) {
+            audio.pause();
+            let prev = document.getElementById(currentAudio);
+            pause(prev);
+        }
+        currentAudio = btn.getAttribute("id");
+        let musicFile = "./assets/" + btn.getAttribute("id");
+        audio = new Audio(musicFile);
+        audio.load();
+        btn.innerHTML = `<i class="fa-regular fa-circle-pause"></i>`;
+        controlBtn.innerHTML = `<i class="fa-regular fa-circle-pause"></i>`;
+        audio.play();
+        isPlaying = true;
+
+        setInterval(function () {
+            let percentage = (audio.currentTime / audio.duration) * 100;
+            progressBar.style.width = `${percentage.toFixed(2)}%`;
+        }, 1000);
+    }
+
+    else {
+        pause(btn);
+        audio.pause();
+    }
+
+}
+
+function pause(btn) {
+    btn.innerHTML = `<i class="fa-regular fa-circle-play"></i>`;
+    controlBtn.innerHTML = `<i class="fa-regular fa-circle-play"></i>`;
+    isPlaying = false;
+}
 
